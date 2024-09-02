aea96fd505e958f3de12cd40f5679beb03432bae
Opeyemi ogungbe <136735745+opeyemiogungbe@users.noreply.github.com>
2024-09-02 05:48:17 -0700

    Update webpack.config.js

--- ecommerce-backend/Build/webpack.config.js
+++ ecommerce-backend/Build/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.js', 
+  target: 'node',
+  externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -14,12 +16,11 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
